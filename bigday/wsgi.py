04aa3a3f49ec09294317c6601293b34accbde95e
bartlomiej.surma <bartlomiej.surma@cispa.saarland>
2019-07-15 11:31:52 +0200

    added localhost to known hosts

--- bigday/wsgi.py
+++ bigday/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('$HOME/public_html/rauan-and-bartek/django-wedding-website')
+sys.path.append('$HOME/public_html/rauan-and-bartek/django-wedding-website/venv/lib/python3.7/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bigday.settings")
 
